65973fa197a88079c38e184cdf44c8a4335fc28b
biswajit dey <bdey862002@gmail.com>
2024-03-27 12:46:18 +0530

    Question 6 is uplode

---
+++ Q6.c
@@ -0,0 +1,14 @@
+// 6.	Write a program to find the largest of three numbers using ternary operators.
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c,max;
+    printf("Enter three numbers : ");
+    scanf("%d%d%d",&a,&b,&c);
+    max = (a>b)? (a>c?a:c) : (b>c?b:c);
+    printf("Largest of three numbers is = %d",max);
+    
+    return 0;
+}
